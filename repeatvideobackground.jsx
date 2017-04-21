a7986fa05c4b4e9331050542fceeaa3b1bd671ae
piperun <nevadadirren@gmail.com>
2017-04-21 08:39:53 +0200

    changed magic numbers to vars

--- repeatvideobackground.jsx
+++ repeatvideobackground.jsx
@@ -1,10 +1,12 @@
 var offset = 48;
 var yoff = 28;
+var nullanum = 41;
+var baselayernum = 40;
 
 x= thisComp.layer(index+1).transform.position[0]+offset;
-if((index+1)%41 != 0) {
+if((index+1)%nullanum != 0) {
 y= thisComp.layer(index+1).transform.position[1];
 } else {
-y = thisComp.layer(40).transform.position[1] + (( index+1) - 41)/41 * yoff;
+y = thisComp.layer(baselayernum).transform.position[1] + (( index+1) - nullanum)/nullanum * yoff;
 }
 [x,y]
