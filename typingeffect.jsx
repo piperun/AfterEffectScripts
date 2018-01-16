bdbeaabfdb882388e7eacba783ca23011be1a71e
piperun <nevadadirren@gmail.com>
2018-01-16 05:59:31 +0100

    Update typingeffect.jsx

--- typingeffect.jsx
+++ typingeffect.jsx
@@ -2,6 +2,6 @@ len = text.sourceText.length;
 speed = time*effect("Speed")("Slider") - effect("Start At")("Slider")*effect("Speed")("Slider");
 F = Math.round(time % 1);
 
-if(F == 1 | (T<len & T>0) ){Fl = "|";}else{Fl=" ";}
+if(F == 1 | (speed<len & speed>0) ){Fl = "|";}else{Fl=" ";}
 
-substr(0,T) + Fl
+substr(0,speed) + Fl
