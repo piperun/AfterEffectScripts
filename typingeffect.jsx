98b5dca0d5d9e826edf67381f9cbc9ac01301ff6
piperun <nevadadirren@gmail.com>
2018-01-16 09:17:14 +0100

    Delete typingeffect.jsx

--- typingeffect.jsx
+++
@@ -1,7 +0,0 @@
-len = text.sourceText.length;
-speed = time*effect("Speed")("Slider") - effect("Start At")("Slider")*effect("Speed")("Slider");
-F = Math.round(time % 1);
-
-if(F == 1 | (speed<len & speed>0) ){Fl = "|";}else{Fl=" ";}
-
-substr(0,speed) + Fl
